2a023497c5ddd5927b0a20bf0959850a97503ed0
Gabrielle Jonsson <dev.gabriellejonsson@gmail.com>
2019-12-11 19:23:01 +0100

    Last build, tested and cleaned - General clean up on code - Added comments to easier navigate the code

--- src/bookStorage.js
+++ src/bookStorage.js
@@ -4,41 +4,45 @@ let addPlease = false;
 
 window.addEventListener('load', () => {
     console.log('Window loaded..')
+    
     /**
      * Event-listeners and queryselectors
      */
-
     let addBookSubmitButton = document.querySelector('#addBookSubmitBtn');
     let addBookAuthor = document.querySelector('#addBookBookAuthor');
     let addBookTitle = document.querySelector('#addBookBookTitle');
+    let viewBooksBtn = document.querySelector('#viewButton')
     let bookContainer = document.querySelector('.bookshelf_books_container');
     let errorContainer = document.querySelector('.error_list_container');
     let successBox = document.querySelector('#successbox');
 
     successBox.checked = false;
+
     addBookSubmitButton.addEventListener('click', addBook);
+    viewBooksBtn.addEventListener('click', fetchBookSelection)
     successBox.addEventListener('change', () => {addPlease = successBox.checked});
 
     /**
-     * Sends GET-request. Used for every GET-request with different query-adds and responshandlers
+     * Sends GET-request. Used for every GET-request with different query parameters and responshandlers
      * @param {string} queries - A query string that is appended to the API-URL.
      * @param {function} handleResponse - A callback function to handle the response from the API.
+     * @param {number} requestCount - The number of requests that have been made in the current sequence. Default value is 1. This parameter should not be provided when invoking the function.
      */
     const sendRequest = (queries, handleResponse, requestCount = 1) => {
         if (addPlease) {
             queries += '&please';
         }
-        console.log('please is: ' + addPlease);
-        
         const xhr = new XMLHttpRequest();
-        xhr.open('GET', apiUrl + queries);                                      // Send GET-request with base-url and the queries specified by parameter
+        // Send GET-request with base-url and the queries specified by parameter
+        xhr.open('GET', apiUrl + queries);
         xhr.onload = function () {
             let response = JSON.parse(this.responseText);
             if (this.status == 200 && response.status == 'success') {
-                handleResponse(response);                              // Initiate the responsehandler specified by parameter, send parsed responsetext as argument to responsehandler.
+                // Invoke the responsehandler specified by parameter, send parsed responsetext as argument to responsehandler.
+                handleResponse(response);
             }
-            // ! This needs to be checked and fixed so that it handles statuscodes other than 200. 
             else {
+                // Invoke errorBuilder with the errormessage, number of requests made and the query sent in the request. Invoke sendRequest recursively if requestCount is less than 5.
                 errorBuilder(response.message, requestCount, queries);
                 if(requestCount < 5) {
                     console.log('Requestcount: ' + requestCount);
@@ -66,19 +70,17 @@ window.addEventListener('load', () => {
         fetchBookSelection();
     } else {
         console.log('User API-key not found in local storage. Requesting a new API-key.')
-        const responseHandlerKey = (response) => {                                          // Function "responsehandler" for generating new API-keys. Take response from xhr.onload as parameter.
-            userApiKey = response.key;                                                      // Set newKey-variable to the value of key in response from xhr.onload.
+        // Handle response when generating new API-keys. Invoke fetchBookSelection to display bookselection of current user.
+        const responseHandlerKey = (response) => {
+            userApiKey = response.key;
             console.log('Saving new key in local storage. Key: ' + userApiKey)
             localStorage.setItem("storedApiKey", userApiKey)
             fetchBookSelection();
         }
-        sendRequest('?requestKey', responseHandlerKey);                                      // Initiate sendRequest-function with query-add requestKey and a responsehandler to handle the response from xhr.onload.
+        sendRequest('?requestKey', responseHandlerKey);
     }
 
-    /**
-     * 
-     */
-    // Create the book
+    // Create the div to hold the bookcontent. Append bookinfo and bookbuttons.
     function createBook(data) {    
         let book = document.createElement('div');
         book.id = data.id;
@@ -88,6 +90,7 @@ window.addEventListener('load', () => {
         return book;
     }
 
+    // Create the unorderedlist to hold author, title and changedata.
     function createBookInfoContainer(authorData, titleData, updatedData) {
         let bookInfoList = document.createElement('ul');
         bookInfoList.className = 'bookshelf_book-info';
@@ -97,7 +100,7 @@ window.addEventListener('load', () => {
         return bookInfoList;
     }
 
-    // Add content to the book
+    // Create the li-elements to hold the value.
     function createBookInfo(data, classname) {
         let bookInfo = document.createElement('li');
         bookInfo.className = classname;
@@ -105,7 +108,7 @@ window.addEventListener('load', () => {
         return bookInfo;
     }
 
-    // Create buttons for the book
+    // Create the unorderedlist to hold edit- and deletebuttons.
     function createBookButtons(bookId) {
         let bookshelfBookButtons = document.createElement('ul');
         bookshelfBookButtons.className = 'bookshelf_book-buttons';
@@ -114,11 +117,18 @@ window.addEventListener('load', () => {
         return bookshelfBookButtons;
     }
 
+    /**
+     * Creates the li-elements to hold the buttons. Creates the button-elements and append to the li-elements.
+     * @param {String} buttontext - Text that will be displayed on the button.
+     * @param {String} classname - Classname of new li-element. Should always be 'bookshelf_book-edit' or 'bookshelf_book-delete'.
+     * @param {Function} listenerType - The function that will be invoked by the eventlistener. Should always be editBook or removeBook.
+     * @param {Number} bookId - ID  of the book that the buttons will belong to.
+     */
     function createButton(buttontext, classname, listenerType, bookId) {
         let btnContainer = document.createElement('li');
         let btn = document.createElement('button');
         let btnClass = classname + 'btn';
-        btnContainer.className = (classname);
+        btnContainer.className = classname;
         btn.setAttribute('bookKey', bookId);
         btn.className = btnClass;
         btn.innerHTML = buttontext;
@@ -127,12 +137,13 @@ window.addEventListener('load', () => {
         return btnContainer;
     }
 
+    // Invoke fetchBookSelection and enable add-button
     function addBookResponseHandler() {
         fetchBookSelection();
         addBookSubmitButton.disabled = false;
     }
 
-    // Send add-book request to API.
+    // Disable add-button and compile query. Invoke sendRequest with query.
     function addBook() {
         addBookSubmitButton.disabled = true;
         let newBookTitle = '&title=' + addBookTitle.value;
@@ -141,11 +152,12 @@ window.addEventListener('load', () => {
         sendRequest(addQuery, addBookResponseHandler);
     }
 
+    // Invoke createBook with a bookobject. Append the book-element created in createBook to the bookContainer-element
     function appendBook(bookData) {
         bookContainer.appendChild(createBook(bookData));
     }
 
-    // Update book-list with information from the API
+    // Clear the current list of books. Take response from sendRequest and iterate through the array of books, send each book to appendBook-function.
     function fetchBookResponseHandler(responseObject) {
         while(bookContainer.hasChildNodes()) {
             bookContainer.removeChild(bookContainer.firstChild)
@@ -153,12 +165,13 @@ window.addEventListener('load', () => {
         responseObject.data.forEach(appendBook);
     }
 
+    // Compile query to fetch current users bookcollection. Invoke sendRequest with compiled query.
     function fetchBookSelection() {
         let fetchQuery = '?op=select&key=' + userApiKey;
         sendRequest(fetchQuery, fetchBookResponseHandler);
     }
 
-
+    // Compile query to delete book. Send request with compiled query.
     function removeBook(event) {
         console.log('Delete button pushed!');
         let buttonID = event.currentTarget.getAttribute('bookkey');
@@ -167,6 +180,7 @@ window.addEventListener('load', () => {
         sendRequest(deleteQuery, fetchBookSelection);
     }
 
+    // Change edit-related values and elements. 
     function editBook(event) {
         console.log('Edit button pushed!');
         let buttonID = event.currentTarget.getAttribute('bookkey')
@@ -177,34 +191,41 @@ window.addEventListener('load', () => {
         event.currentTarget.innerHTML = 'SAVE';
         event.currentTarget.addEventListener('click', saveEditedBook);
 
-        // Save text-fields from previous book
+        // Create variables to be used for saving current value of elements.
         let authorElement = document.getElementById(buttonID).querySelector('.bookshelf_book-author');
-        let authorOldText = authorElement.innerHTML;
         let titleElement = document.getElementById(buttonID).querySelector('.bookshelf_book-title');
-        let titleOldText = titleElement.innerHTML;
         let updateElement = document.getElementById(buttonID).querySelector('.bookshelf_book-changed');
 
+        // Save current value of elements from previous book
+        let authorOldText = authorElement.innerHTML;
+        let titleOldText = titleElement.innerHTML;
+
         // Remove li-elements containing outdated book-info
-        authorElement.remove();
-        titleElement.remove();
-        updateElement.remove();
+        removeBookInfo(authorElement, titleElement, updateElement);
 
-        // Add input-fields for changes
-        let newAuthorInput = document.createElement('input');
-        newAuthorInput.placeholder = authorOldText;
-        newAuthorInput.value = authorOldText;
-        newAuthorInput.setAttribute('required', '');
-        newAuthorInput.className = 'newAuthor';
-        bookInfoContainer.appendChild(newAuthorInput);
+        // Append new input-fields built in buildEditInputElements
+        bookInfoContainer.appendChild(createEditInputElement('Author', authorOldText));
+        bookInfoContainer.appendChild(createEditInputElement('Title', titleOldText));
+    }
 
-        let newTitleInput = document.createElement('input');
-        newTitleInput.placeholder = titleOldText;
-        newTitleInput.value = titleOldText;
-        newTitleInput.setAttribute('required', '');
-        newTitleInput.className = 'newTitle';
-        bookInfoContainer.appendChild(newTitleInput);
+    // Remove elements related to edited book.
+    function removeBookInfo(author, title,  update) {
+        author.remove();
+        title.remove();
+        update.remove();
+    }
+
+    // Create input-element for editing bookinfo
+    function createEditInputElement(inputCategory, oldText) {
+        let newInputElem = document.createElement('input');
+        newInputElem.placeholder = oldText;
+        newInputElem.value = oldText;
+        newInputElem.setAttribute('required', '');
+        newInputElem.className = 'new' + inputCategory;
+        return newInputElem;
     }
     
+    // Save changes made to book into variables. Compile query for editing book. If new variables have a value, invoke sendRequest with compiled query.
     function saveEditedBook(event) {
         let buttonID = event.currentTarget.getAttribute('bookkey')
         let newAuthorValue = document.querySelector('.newAuthor').value;
@@ -221,12 +242,10 @@ window.addEventListener('load', () => {
     }
 
     function errorBuilder(message, requestCount, queries) {
-        console.log('We\'re inside errorBuilder');
-        console.log(message);
+        // Activate add-button after 5 failed requests.
         if(requestCount == 5) {
             addBookSubmitButton.disabled = false;
         }
-                
         // Create the UL for errorinformation
         let newError = document.createElement('ul');
         newError.className = 'error_list_item';
